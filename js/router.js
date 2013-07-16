0923234ac18c0f08a85233197e5e06b84a32aec7
mo khan <mo@mokhan.ca>
2013-07-16 09:40:01 -0600

    add missing markup, css and image

---
+++ js/router.js
@@ -0,0 +1,10 @@
+
+Todos.Router.map(function(){
+  this.resource('todos', { path: '/' });
+});
+
+Todos.TodosRoute = Ember.Route.extend({
+  model: function(){
+    return Todos.Todo.find();
+  }
+});
