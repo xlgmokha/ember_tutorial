d10c4291d0fdad31b0c1a0f391176c6a458cad2e
mo khan <mo@mokhan.ca>
2013-07-16 10:12:19 -0600

    double click to edit a todo

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -8,5 +8,9 @@ Todos.TodoController = Ember.ObjectController.extend({
       model.save();
       return value;
     }
-  }.property('model.isCompleted')
+  }.property('model.isCompleted'),
+  isEditing: false,
+  editTodo: function() {
+    this.set('isEditing', true);
+  },
 });
