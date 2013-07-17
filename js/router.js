ab031c283dab90baad756d039259a34317815de2
mo khan <mo@mokhan.ca>
2013-07-17 14:51:13 -0600

    extract out nested template

--- js/router.js
+++ js/router.js
@@ -1,9 +1,17 @@
 Todos.Router.map(function(){
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function() {
+
+  });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
   model: function(){
+    return Todos.Todo.find();
+  }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
     return Todos.Todo.find();
   }
 });
