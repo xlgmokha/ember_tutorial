0c8521a715de976fe253e6ae59f6ed5ae41a2664
mo khan <mo@mokhan.ca>
2013-07-17 15:03:59 -0600

    add route to completed todos

--- js/router.js
+++ js/router.js
@@ -1,6 +1,7 @@
 Todos.Router.map(function(){
   this.resource('todos', { path: '/' }, function() {
     this.route('active');
+    this.route('completed');
   });
 });
 
@@ -20,6 +21,17 @@ Todos.TodosActiveRoute = Ember.Route.extend({
   model: function() {
     return Todos.Todo.filter(function(todo) {
       if (!todo.get('isCompleted')) { return true; }
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', { controller: controller });
+  }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function() {
+    return Todos.Todo.filter(function(todo){
+      if (todo.get('isCompleted')) { return true; }
     });
   },
   renderTemplate: function(controller) {
