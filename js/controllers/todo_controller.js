fd63738ba3ea92c5295a833f28482074dcc2fc9b
mo khan <mo@mokhan.ca>
2013-07-17 14:45:01 -0600

    connect remove button to remove a todo

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -17,4 +17,9 @@ Todos.TodoController = Ember.ObjectController.extend({
     this.set('isEditing', false);
     this.get('model').save();
   },
+  removeTodo: function(){
+    var todo = this.get('model');
+    todo.deleteRecord();
+    todo.save();
+  }
 });
